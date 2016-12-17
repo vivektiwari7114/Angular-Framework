c0c21eb94fed15cdf3dc04d66d5a7b02a55da8cf
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-12-17 17:28:33 +0530

    Angular MVC Framework

---
+++ app/scripts/controllers/menu.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udaciMealsApp.controller:MenuCtrl
+ * @description
+ * # MenuCtrl
+ * Controller of the udaciMealsApp
+ */
+angular.module('udaciMealsApp')
+  .controller('MenuCtrl', function () {
+        this.id = 'Hey';
+        this.name = 'How';
+        this.img = 'yeoman.png';
+        this.rating = 10;
+        this.items = ['1', '2', '3', '4', '5'];
+  });
